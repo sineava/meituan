e46f94e5823fc9f83c6200d24161ce8a2db66589
sineava <1690150504@qq.com>
2020-04-08 10:11:07 +0800

    评分组件

--- src/components/star/Star.vue
+++ src/components/star/Star.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="star">
-        <span v-for="(star, index) in stars" :key="index">
+        <span v-for="(star, index) in itemClasses" :key="index">
             <img :src="star == 'star_on' ? star_on : star == 'star_half' ? star_half : star_off">
         </span>
     </div>
@@ -10,28 +10,31 @@
 export default {
     data () {
         return {
-            stars: [],
             star_on: require('../../assets/img/star24_on@2x.png'),
             star_half: require('../../assets/img/star24_half@2x.png'),
             star_off: require('../../assets/img/star24_off@2x.png')
         }
     },
-    created () {
-        let score = this.score
-        const totalstar = this.totalstar
-        if (isNaN(score) && score < 0) return
-        // 避免js的奇葩问题
-        score = score * 10 % 10 >= 5 ? Number.parseInt(score) + 0.5 : Number.parseInt(score)
-        const half = score * 10 % 10 === 5 ? 1 : 0
-        const on = Number.parseInt(score)
-        this.stars.length = totalstar
-        this.stars.fill('star_off')
-        for (let i = 0, flag = false; i < on; i++) {
-            if (!flag && half === 1) {
-                this.stars[on] = 'star_half'
-                flag = true
+    computed: {
+        itemClasses () {
+            const stars = []
+            let score = this.score
+            const totalstar = this.totalstar
+            if (isNaN(score) && score < 0) return
+            // 避免js的奇葩问题
+            score = score * 10 % 10 >= 5 ? Number.parseInt(score) + 0.5 : Number.parseInt(score)
+            const half = score * 10 % 10 === 5 ? 1 : 0
+            const on = Number.parseInt(score)
+            stars.length = totalstar
+            stars.fill('star_off')
+            for (let i = 0, flag = false; i < on; i++) {
+                if (!flag && half === 1) {
+                    stars[on] = 'star_half'
+                    flag = true
+                }
+                stars[i] = 'star_on'
             }
-            this.stars[i] = 'star_on'
+            return stars
         }
     },
     props: {
