49cd6d1ee0a5d35d3baa3c5a4ef5921039c8ac07
sineava <1690150504@qq.com>
2020-04-09 17:56:30 +0800

    入口文件

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,53 @@
 <template>
-  <div id="app">
-      <Home></Home>
-  </div>
+    <div id="app">
+        <!-- 头部 -->
+        <Myheader :poi_info="poi_info"/>
+        <!-- 导航 -->
+        <Mynav :comment_num="comment_num"/>
+        <!-- 主体内容 -->
+        <keep-alive>
+            <router-view></router-view>
+        </keep-alive>
+    </div>
 </template>
 
 <script>
 export default {
+    data () {
+        return {
+            poi_info: {},
+            comment_num: 0,
+            rating: {}
+        }
+    },
     components: {
-        Home: () => import('./views/Home')
+        Myheader: () => import('./components/header/Header'),
+        Mynav: () => import('./components/nav/Nav')
+    },
+    created () {
+        // 请求goods数据
+        this.$axios.get('/api/goods')
+            .then(data => {
+                const res = data.data
+                if (res.code !== 0) return
+                this.poi_info = res.data.poi_info
+            })
+            .catch(err => console.log(err))
+        // 请求评分数据
+        this.$axios.get('/api/rating')
+            .then(data => {
+                const res = data.data
+                if (res.code !== 0) return
+                this.comment_num = res.data.comment_num
+            })
+            .catch(err => console.log(err))
     }
 }
 </script>
+
+<style lang="less">
+#app {
+    width: 100vw;
+    overflow: hidden;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,43 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+
+// 引入全局字体
+import './common/styles/icon.css'
+// 引入css样式重置
+import './assets/css/reset.css'
+
+// vue消息提示组件
+import Message from 'vue-m-message'
+import 'vue-m-message/dist/index.css'
+
+// moment
+import moment from 'moment'
+
+// import img-vuer and install
+import gallery from 'img-vuer'
+Vue.use(gallery, {
+    swipeThreshold: 150, // default 100
+    isIndexShow: true, // show image index, default true
+    useCloseButton: true, // trigger gallery close with close button, default true
+    preload: true // preload images in the same group, default true
+})
+
+Vue.use(Message)
 
 // 阻止启用生产消息
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'http://192.168.31.73:8000/' : 'http://47.56.13.69:8000'
+
+// 时间格式化
+Vue.filter('dateFormat', (dateStr, pattern = 'YYYY-MM-DD') => {
+    return moment(dateStr).format(pattern)
+})
+
+// 绑定原型
+Vue.prototype.$axios = axios
 
 new Vue({
     router,
